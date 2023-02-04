3cdac3ce70b6038c4becd45af3d9704cde5ae70d
marlonfloresramos <marlon.floresramos07@gmail.com>
2023-02-04 11:53:06 -0500

    Se configura la vista inicial

--- ClimaApp/ViewController.swift
+++ ClimaApp/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ciudades: String = "Lima"
+    var temperatura: String = "23°C"
 
+    @IBOutlet weak var labelWeather: UILabel!
+    @IBOutlet weak var searchWeather: UIButton!
+    @IBOutlet weak var tempWeather: UILabel!
+    @IBOutlet weak var textWeather: UITextField!
+    @IBOutlet weak var titleWeather: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        labelWeather.text = ""
+        tempWeather.text = ""
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func searchButton(_ sender: Any) {
+        tempWeather.text = temperatura
+        labelWeather.text = textWeather.text
+    }
 }
+    
+
 
